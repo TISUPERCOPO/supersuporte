905a926b1daf5b0c43cc4ede2d11cb29e0bd0053
RomarioRibeiro <romaryjane@hotmail.com>
2023-08-29 17:35:07 -0300

    projeto de suporte para usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { AccordionModule } from 'primeng/accordion';
+import { HeaderComponent } from './componetes/templates/header/header.component';
+import { FooterComponent } from './componetes/templates/footer/footer.component';
+import { ConteudoComponent } from './componetes/templates/conteudo/conteudo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConteudoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
